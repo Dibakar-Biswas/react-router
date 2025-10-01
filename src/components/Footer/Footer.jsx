62c665c6fd748e7bd57ba00858bb1d6f99b77a1c
Dibakar-Biswas <dibakarbiswas742@gmail.com>
2025-10-01 22:31:47 +0600

    first commit

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            <h2><small>Thanks for visiting our website</small></h2>
+        </div>
+    );
+};
+
+export default Footer;<h2>Thanks for visiting our website</h2>
